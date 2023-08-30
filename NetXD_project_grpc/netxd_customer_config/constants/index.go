a392fd1862d29496aba4406f90a993add134a897
KISHORE NS <102172168+kishorens18@users.noreply.github.com>
2023-08-30 13:29:33 +0530

    Delete NetXD_project_grpc directory

--- NetXD_project_grpc/netxd_customer_config/constants/index.go
+++
@@ -1,7 +0,0 @@
-package constants
-
-const ConnectionString = ""
-
-const Port = ":8080"
-
-const DBName = "BANK"
--- NetXD_project_grpc/netxd_customer_dal/interfaces/netxd_dal_interface.go
+++
@@ -1,7 +0,0 @@
-package interfaces
-
-import "github.com/kishorens18/NetXD_Project/netxd_customer_dal/models"
-
-type ICustomer interface {
-	CreateCustomer(customer *models.Customer) (*models.DBResponse, error)
-}
